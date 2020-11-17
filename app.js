09cb4afc883090435eac5e6a3cbe9863e5e29031
Marcus Järveskog <abr.metalcore98@gmail.com>
2020-11-17 16:34:58 +0100

    almost done, fixed error message

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ form.addEventListener('submit', function(e){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
 
     //kalla på och hämta data från APIn
+    let errorMessage = document.querySelector('.error')
     fetch(url).then(
         function(response){
             return response.json();
@@ -23,44 +24,53 @@ form.addEventListener('submit', function(e){
             console.log(data)
             // deklarera all data jag behöver som variblar
             let name = data.name;
-            let icon;
+            let icon = data.weather[0].icon;
             let description = data.weather[0].description;
             let temp = data.main.temp;
             let windSpeed = data.wind.speed;
             let humidity = data.main.humidity;
 
-            //selecta de element där jag vill lägga till datan i DOM
-            //temperatur
-            let tempBig = document.querySelector('.header .temp');
-            let tempSmall = document.querySelector('.info-list .temp');
+            addDataToDom(name,icon,description,temp,windSpeed,humidity);
 
-            tempBig.innerHTML = `${temp}<span>°</span>`;
-            tempSmall.innerText = temp;
-
-            //vädrets beskrivning
-            let weatherElement = document.querySelector('.weather');
-            weatherElement.innerText = description;
-
-            //stadens namn
-            let nameElement = document.querySelector('.city-name');
-            nameElement.innerText = name;
-
-            //vind hastighet
-            let windElement = document.querySelector('.wind');
-            windElement.innerText = windSpeed + 'm/s'
-
-            //luftfuktighet
-            let humidityElement = document.querySelector('.humidity');
-            humidityElement.innerText = humidity + '%';
-
+            errorMessage.innerText = ""
 
         }
     ).catch(function(error){
-        console.log('ett fel uppstod med url.', error)
+        console.log('the city was not found, check spelling')
+        
+        errorMessage.innerText = "the city was not found, check spelling"
+    
     });
-
 
 })
 
 
    
+//selecta element i DOM och lägg till den hämtade datan i dem
+function addDataToDom(n,i,d,t,ws,h){
+    //temperatur
+    let tempBig = document.querySelector('.header .temp');
+    let tempSmall = document.querySelector('.info-list .temp');
+    tempBig.innerHTML = `${t}<span>°</span>`;
+    tempSmall.innerText = t;
+
+    //vädrets beskrivning
+    let weatherElement = document.querySelector('.weather');
+    weatherElement.innerText = d;
+
+    //stadens namn
+    let nameElement = document.querySelector('.city-name');
+    nameElement.innerText = n;
+
+    //väder ikonen
+    let iconElement = document.querySelector('.title .icon');
+    iconElement.src = `http://openweathermap.org/img/wn/${i}@2x.png`
+
+    //vind hastighet
+    let windElement = document.querySelector('.wind');
+    windElement.innerText = ws + 'm/s'
+
+    //luftfuktighet
+    let humidityElement = document.querySelector('.humidity');
+    humidityElement.innerText = h + '%';
+}
