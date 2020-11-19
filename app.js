070e2719f1b672da04e7fa52c1c7fb7f8dbe3459
Marcus Järveskog <abr.metalcore98@gmail.com>
2020-11-19 16:52:08 +0100

    appen är nu klar enligt kunskapskraven, experimetala ändringar kan komma efter

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ form.addEventListener('submit', function(e){
     const apiKey = 'aff11d30b0c98d57fd78cc74bb6fa6ad'
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
 
+
     //kalla på och hämta data från APIn
     let errorMessage = document.querySelector('.error')
     fetch(url).then(
@@ -21,23 +22,23 @@ form.addEventListener('submit', function(e){
         }
     ).then(
         function(data){
-            console.log(data)
             // deklarera all data jag behöver som variblar
             let name = data.name;
             let icon = data.weather[0].icon;
             let description = data.weather[0].description;
-            let temp = data.main.temp;
+            let temp = Math.floor(data.main.temp);
             let windSpeed = data.wind.speed;
             let humidity = data.main.humidity;
 
             addDataToDom(name,icon,description,temp,windSpeed,humidity);
-
+            changeBackground(temp);
+            //ta bort error text ifall där är någon
             errorMessage.innerText = ""
-
+            
         }
     ).catch(function(error){
-        console.log('the city was not found, check spelling')
         
+        //lägg till en error text
         errorMessage.innerText = "the city was not found, check spelling"
     
     });
@@ -45,7 +46,6 @@ form.addEventListener('submit', function(e){
 })
 
 
-   
 //selecta element i DOM och lägg till den hämtade datan i dem
 function addDataToDom(n,i,d,t,ws,h){
     //temperatur
@@ -65,6 +65,7 @@ function addDataToDom(n,i,d,t,ws,h){
     //väder ikonen
     let iconElement = document.querySelector('.title .icon');
     iconElement.src = `http://openweathermap.org/img/wn/${i}@2x.png`
+    iconElement.style.display = 'block';
 
     //vind hastighet
     let windElement = document.querySelector('.wind');
@@ -73,4 +74,18 @@ function addDataToDom(n,i,d,t,ws,h){
     //luftfuktighet
     let humidityElement = document.querySelector('.humidity');
     humidityElement.innerText = h + '%';
+}
+
+
+
+// en funktion som byter bakgrund beroende på vad temperaturen är
+function changeBackground(temp){
+    let appContainer = document.querySelector('.app-container');
+    if(temp <= 5){
+        appContainer.style.backgroundImage = "url('../img/cold-img.jpg')"
+    } else if(temp > 5 && temp <= 20){
+        appContainer.style.backgroundImage = "url('../img/autumn-img.jpg')"
+    } else if(temp > 20){
+        appContainer.style.backgroundImage = "url('../img/summer-img.jpg')"
+    }
 }
